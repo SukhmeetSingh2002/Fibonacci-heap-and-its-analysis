74a6f009ab3babae01e019dabfdadab63cb379aa
SukhmeetSingh <78161187+SukhmeetSingh2002@users.noreply.github.com>
2021-11-10 19:27:43 +0530

    Added insert function of fibb heap

--- main.c
+++ main.c
@@ -20,20 +20,50 @@ typedef struct heap
     int n;
 } heap;
 
+heap *fibHeap;
+
 heap *MAKE_HEAP()
 {
 }
 
-node* allocateMemoryNode()
+node *allocateMemoryNode()
 {
-    node* temp=(node* )malloc(sizeof(node));
-    temp->degree=0;
-    temp->parent=NULL;
-    temp->child=NULL;
+    node *temp = (node *)malloc(sizeof(node));
+    temp->degree = 0;
+    temp->parent = NULL;
+    temp->child = NULL;
+    temp->mark = false;
     return temp;
 }
 
+void fib_heap_insert(int key)
+{
+    node *newNode = allocateMemoryNode();
+    newNode->key = key;
+    if (fibHeap->min == NULL)
+        fibHeap->min = newNode;
+    else
+    {
+        if (fibHeap->min->right == NULL)
+        {
+            fibHeap->min->right = newNode;
+            newNode->left=fibHeap->min;
+            newNode->right=fibHeap->min;
+        }
+        else
+        {
+            newNode->right=fibHeap->min->right;
+            newNode->left=fibHeap;
+            fibHeap->min->right->left=newNode;
+            fibHeap->min->right=fibHeap;
+        }
+        if(newNode->key<fibHeap->min->key)
+            fibHeap->min=newNode;
+    }
+    fibHeap->n=fibHeap->n+1;
+}
 
 int main()
 {
+    fibHeap = (heap *)malloc(sizeof(heap));
 }
