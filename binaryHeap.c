2625cbff87677a2c915aad7d3cf0930e9e0881a6
SukhmeetSingh <78161187+SukhmeetSingh2002@users.noreply.github.com>
2021-11-16 20:10:44 +0530

    Adds extract min for binary heap

--- binaryHeap.c
+++ binaryHeap.c
@@ -18,14 +18,14 @@ int parent(int *a,int i)
 }
 
 
-void minHeapyfy(int *a,int i,int heapSize)
+void minHeapyfy(int *a,int i,int *heapSize)
 {
     int leftChild=left(a,i),rightChild=right(a,i),temp=0,smallest=i;
-    if(leftChild<heapSize && a[i]>a[leftChild])//left child is smaller
+    if(leftChild<*heapSize && a[i]>a[leftChild])//left child is smaller
     {
         smallest=leftChild;
     }
-    if(rightChild<heapSize && a[smallest]>a[rightChild])//right child is smaller
+    if(rightChild<*heapSize && a[smallest]>a[rightChild])//right child is smaller
     {
         smallest=rightChild;
     }
@@ -38,15 +38,31 @@ void minHeapyfy(int *a,int i,int heapSize)
     }
 }
 
-void builMinHeap(int *a,int heapSize)
+void builMinHeap(int *a,int *heapSize)
 {
-    for (int i = heapSize; i >= 0; i--)
+    for (int i = *heapSize; i >= 0; i--)
     {
         minHeapyfy(a,i,heapSize);
     }
     
 }
 
+int extractMin(int *a,int *heapSize)
+{
+    int minElt;
+    if(*heapSize<0)
+    {
+        printf("Underflow\n");
+    }
+    else
+    {
+        minElt=a[0];
+        a[0]=a[*heapSize-1];
+        *heapSize--;
+        minHeapyfy(a,0,heapSize);
+    }
+    return minElt;
+}
 
 int main()
 {
